0502c48653c64e9d8430ec2382414d17cb2c7ed2
Connor Weitkowitz <cweitkowitz@academic.rrc.ca>
2025-05-09 14:33:09 -0500

    Updated README and py to be more specific about what exactly it is about. Added first set of question to py.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## Description
-
 In this project I will conduct an exhaustive list of Python practice
 problems that will only ever increase in scope and complexity.
 The intention of this project is to maintain and even grow my skills
@@ -8,4 +7,10 @@ consistency and then addign quality and time over the daily session
 
 ## Author
 Connor Weitkowitz
-## Add more if needed
+
+## practice_problems.py
+This py includes all of the practice problems included in
+module 2 of SDF and will also include other problems related to
+module 2
+
+## ADD NAME HERE
--- practice_problems.py
+++ practice_problems.py
@@ -1,6 +1,6 @@
 """
-In this module I will conduct the first SDF practice problems and
-perhaps more if I find my knowledge lacking? 
+In this module I will conduct the first SDF practice problems found in 
+module 2.
 
 Simple python problems designed to keep my knowledge of Python
 up to date.
@@ -8,3 +8,18 @@ up to date.
 
 ___author___ = "Connor Weitkowitz"
 ___version___ = "09/05/2025"
+
+"""
+Given the following variables with corresponding values:
+name = "Jo-Anne Sinclair"
+salary = 123093
+1. Define a print statement that will produce the following output:
+Jo-Anne Sinclair: 123093
+2. Use an f-string to produce the following output:
+Name: Jo-Anne Sinclair
+Salary: $123,093.00
+
+"""
+
+# 1. Define a print statement that will produce the following output:
+#    Jo-Anne Sinclair: 123093
