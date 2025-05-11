745ed561adc78675a2486f49014926f4dd2afbf8
Connor Weitkowitz <cweitkowitz@academic.rrc.ca>
2025-05-11 14:54:25 -0500

    Added everything in module_3_problems into a basic function to enact a mainguard as to prevent it from running in other scripts.

--- Module_3_Problems.py
+++ Module_3_Problems.py
@@ -10,12 +10,40 @@ ___version___ = "11/05/2025"
 
 from Module_2_Problems import question_break
 
-"""
-1.  Write a program that generates two random numbers between 0 and 100 
-    and prints the smaller of the two numbers.
-"""
 
-question_break
+def module_3_problems():
+    """
+    1.  Write a program that generates two random numbers between 0 and 100 
+        and prints the smaller of the two numbers.
+    """
 
-"""
-"""
+    question_break
+
+    """
+    2.  Write a program that takes a float as an input, then computes and outputs
+        the cube of that number using the pow method of the math module.
+    """
+
+    question_break
+
+    """
+    3.  Write a program that reads from the keyboard the radius of a circle. 
+    Calculate and output the area and the circumference of that circle. 
+    You can use the following formulas: math formula goes here haha :3c
+    """
+
+    question_break
+
+    """
+    4.	Write a program that generates five random integers between 60 and 
+    100 and calculates the smallest of the five numbers.
+    """
+
+    question_break
+
+    """
+    5.	Write a program that generates three random integers between 0 and 
+    50, calculates the average, and prints the result to one decimal place.
+    """
+if __name__ == '__Main__':
+    module_3_problems()
